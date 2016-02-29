dab8d58606fe41b6c360b1083fa39a7e867e5cd2
ckinkeldey <christoph.kinkeldey@gmx.de>
2016-02-29 16:38:58 +0100

    Fixed project setup

--- ckanext/ccca/plugin.py
+++ ckanext/ccca/plugin.py
@@ -1,9 +1,19 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.lib.base as base
+from pylons import g, c
+from jinja2 import Environment, FileSystemLoader
+from os import listdir
+from os.path import isfile, join, expanduser
 
+import logging
+import json
+
+log = logging.getLogger(__name__)
 
 class CccaPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IConfigurer, inherit=True)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +21,27 @@ class CccaPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ccca')
+        
+    def before_map(self, map):
+        map.connect('sftp_upload', '/sftp_upload', controller='ckanext.ccca.plugin:UploadController', action='show_filelist')
+        return map
+    
+    
+    def after_map(self, map):
+        #log.fatal("==================================> %s" % map)
+        return map
+
+class UploadController(base.BaseController):
+    
+    def show_filelist(self):
+        user = c.userobj
+        data = base.request.params
+        log.debug(user)
+        if 'apikey' in data and data['apikey']==user.apikey:
+            mypath = expanduser("~")+'/../'+user.name+'/ckan/'
+            onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and not f.startswith('.'))]
+            log.debug(c.userobj.name)
+            return json.dumps(onlyfiles)
+        else:
+             return "no API key"
+        
