9899b471b74f1aef2fbef964abe0587ca744b008
anja.stemme@zamg.ac.at <anja.stemme@zamg.ac.at>
2018-04-18 15:50:50 +0200

    Show "order-by" for organizations; check news dataset for different formats in *all* versions

--- ckanext/ccca/controllers/about.py
+++ ckanext/ccca/controllers/about.py
@@ -20,11 +20,25 @@ ValidationError = logic.ValidationError
 from ckanext.ccca import helpers as hc
 import json
 
+def _check_existing_formats(ff, list_of_formats):
+
+    for x in list_of_formats:
+        for d in x:
+            if d['format'] == ff:
+                return True
+            else:
+                break
+    return False
+
+
 class AboutController(p.toolkit.BaseController):
     """
     Controller for displaying about pages
     """
     def news_archive(self):
+
+        #Show list of news: Abstract of newest resource per version
+        #Display Archive: One File/Link per Format per Year - Year identified via name
 
         news_id  = hc.ccca_check_news()
 
@@ -32,113 +46,109 @@ class AboutController(p.toolkit.BaseController):
             return
         try:
             news_pkg = tk.get_action('package_show')({}, {'id': news_id, 'include_datasets':True})
+        except:
+            print "No News Dataset founds ..."
+            return
 
-            #get a list with news messages
-            news_list = []
-            #get the newest files with different resource titles
-            files = []
-            d_titles = []
+        #get a list with news messages
+        news_list = []
+        #get the newest files with different resource titles
+        formats_and_files = []
+        d_titles = []
 
-            nd = ''
-            newest_res = {}
-            newest_title = ''
-            # consider distributions and look for latest change
-            newest_res = {}
-            #FIXME: Wenn neueste Version nur eine Distribuion hat, werden die alten nicht angezeigt...
-            for x in news_pkg['resources']:
-                #latest change
-                if x['last_modified'] > nd:
-                    nd = x['last_modified']
-                    newest_res = x
-                    newest_title = x['name']
-                #consider distributions
-                f = []
-                f_entry = {}
-                f_entry['url'] = x['url']
-                f_entry['name'] = x['name']
-                f_entry['format'] = x['format']
-                f.append(f_entry)
-                files.append(f)
+        nd = ''
+        newest_res = {}
+        newest_title = ''
+        # consider distributions and look for latest change
+        newest_res = {}
+        # Build initial formatlist
+        for x in news_pkg['resources']:
+            #latest change
+            if x['created'] > nd or  x['last_modified'] > nd:
+                nd = x['last_modified'] if x['last_modified'] else x['created']
+                newest_res = x
+                newest_title = x['name']
+            #consider distributions
+            f = []
+            f_entry = {}
+            f_entry['url'] = x['url']
+            f_entry['name'] = x['name']
+            f_entry['format'] = x['format']
+            if x['format'] == '': # presumably URL
+                f_entry['format'] = "Link"
 
+            f.append(f_entry)
+            formats_and_files.append(f)
 
-            news_list.append(newest_res)
+        news_list.append(newest_res)
 
-            #Look for older versions and append them to list for template
-            relations = news_pkg['relations']
-            if len(relations) > 0:
-                has_older_versions = True
-            else:
-                has_older_versions = False
+        #Look for older versions and append them to list for template
+        relations = news_pkg['relations']
+        if len(relations) > 0:
+            has_older_versions = True
+        else:
+            has_older_versions = False
 
-            while has_older_versions:
+        while has_older_versions:
 
-                for x in relations:
-                    if x['relation'] == 'is_version_of':
-                        older_version= x['id']
-                        news_pkg = tk.get_action('package_show')({}, {'id':older_version, 'include_datasets':True})
+            for x in relations:
+                if x['relation'] == 'is_version_of':
+                    older_version= x['id']
+                    news_pkg = tk.get_action('package_show')({}, {'id':older_version, 'include_datasets':True})
 
-                        # implicates that all distributions have the same description!
-                        news_list.append(news_pkg['resources'][0])
+                    # implicates that all distributions have the same description!
+                    news_list.append(news_pkg['resources'][0])
 
-                        #Consider distributions and titles#
-                        #FIXME: Might lead to a durcheinander if distributions are not always in the same order....
-                        # and different numbers of distributions.... and latest change not first in list ....
-                        #check dict entrys...
-                        if news_pkg['resources'][0]['name'] != newest_title:
-                             newest_title = news_pkg['resources'][0]['name']
-                             index = 0
-                             for x in news_pkg['resources']:
-                                 #print "hi"
-                                 f_entry = {}
-                                 f_entry['url'] = x['url']
-                                 f_entry['name'] = x['name']
-                                 f_entry['format'] = x['format']
-                                 for fl in files:
+                    #Search formats years
+                    for res in news_pkg['resources']:
+                         f_entry = {}
+                         f_entry['url'] = res['url']
+                         f_entry['name'] = res['name']
+                         f_entry['format'] = res['format']
+                         if res['format'] == '': # presumably URL
+                              f_entry['format'] = 'Link'
+                         if not _check_existing_formats(f_entry['format'], formats_and_files):
+                             # New list of files for format
+                             ff = []
+                             ff.append(f_entry)
+                             formats_and_files.append(ff)
+                         else:
+                             if res['name'] != newest_title: #one file per format per year
+                                 newest_title = res['name']
+                                 for fl in formats_and_files:
                                      for f in fl:
                                          if f['format'] == f_entry['format']:
                                             fl.append(f_entry)
                                             break
-                                 index +=1
-                        # NExt loop
-                        relations = news_pkg['relations']
-                        if len(relations) > 0:
-                            has_older_versions = True
-                        else:
-                            has_older_versions = False
-                    elif x['relation'] == 'has_version' and len(relations) == 1 :
-                        has_older_versions = False
-                        break
+                        # Next loop
+                    relations = news_pkg['relations']
+                    if len(relations) > 0:
+                        has_older_versions = True
                     else:
-                        continue
-
-        except:
-            print "des war nix ..."
-            return
-
-        #print files
-
-        return p.toolkit.render('about/news_archive.html', {'title': 'News Archive', 'news': news_list, 'files': files })
+                        has_older_versions = False
+                elif x['relation'] == 'has_version' and len(relations) == 1 :
+                    has_older_versions = False
+                    break
+                else:
+                    continue
 
 
-        # Reverse order
-        news_list = list(reversed(news_list))
 
-        # Remove versions and save urls and titles of older archives
-        files = []
-        for x in news_list:
-            if 'newer_version' in x and x['newer_version'] == "":
-                f_entry = {}
-                f_entry['url'] = x['url']
-                f_entry['name'] = x['name']
-                files.append(f_entry)
-                #print x['name']
+        #print formats_and_files
 
-        #print files
-        #print json.dumps(news_res_list, indent=3)
-        #print json.dumps (list(reversed(news_res_list)),indent=3)
-        #print json.dumps(files, indent=3)
+        # Link as last item(s)
+        sorted_format_and_files =[]
+        for ff in formats_and_files:
+            if ff[0]['format'] != 'Link':
+                sorted_format_and_files.append(ff)
+        for ff in formats_and_files:
+            if ff[0]['format'] == 'Link':
+                sorted_format_and_files.append(ff)
 
-        return p.toolkit.render('about/news_archive.html', {'title': 'News Archive', 'news': news_list, 'files': files })
+        print "FINAL LIst"
+        print sorted_format_and_files
+
+        return p.toolkit.render('about/news_archive.html', {'title': 'News Archive', 'news': news_list, 'files': sorted_format_and_files })
 
     def usage(self):
         return p.toolkit.render('about/usage.html', {'title': 'Usage Information'})
